2f4416c6e0253d6141f408812ae9e6a438576ecd
Kareem Yousef Abu Mosameh‬‎ <123791815+kareem-mosameh@users.noreply.github.com>
2025-05-17 22:35:15 +0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let allPA = document.getElementById("all_pa")
+
 let count = 0
+let allpasenger = 0
 
 function increment() {
     count += 1
@@ -10,6 +13,8 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    allpasenger += count
+    allPA.innerText = "all pasengers: " + allpasenger
     countEl.textContent = 0
     count = 0
 }
